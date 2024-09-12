28b466fa6d38c5a4f988d88edaae328a5bc7eda8
godx <1452366179@qq.com>
2024-09-12 14:50:03 +0800

    现在samGTE可以达到68.39

--- metrics/apls_for_samGTE.bash
+++ metrics/apls_for_samGTE.bash
@@ -2,20 +2,20 @@ declare -a arr=(8 9 19 28 29 39 48 49 59 68 69 79 88 89 99 108 109 119 128 129 1
 # declare -a arr=(49 179)
 
 # source directory
-dir=./save/GTE_加了噪声_59epoch/decode_result
+dir=./save/GTE_加了噪声并调整损失权重_37epoch/decode_result
 
 echo "预测结果保存路径：${dir}"
 mkdir -p ../$dir/results/apls
 # now loop through the above array
-for i in "${arr[@]}"   
+# for i in "${arr[@]}"   
 
-do
-    if test -f "../${dir}/region_${i}_graph.p"; then
-        echo "========================$i======================"
-        python ./apls/convert.py "../cityscale/20cities/region_${i}_graph_gt.pickle" gt.json
-        python ./apls/convert.py "../${dir}/region_${i}_graph.p" prop.json
-        go run ./apls/main.go gt.json prop.json ../$dir/results/apls/$i.txt 
-    fi
-done
+# do
+#     if test -f "../${dir}/region_${i}_graph.p"; then
+#         echo "========================$i======================"
+#         python ./apls/convert.py "../cityscale/20cities/region_${i}_graph_gt.pickle" gt.json
+#         python ./apls/convert.py "../${dir}/region_${i}_graph.p" prop.json
+#         go run ./apls/main.go gt.json prop.json ../$dir/results/apls/$i.txt 
+#     fi
+# done
 
 python3 apls.py --dir $dir
